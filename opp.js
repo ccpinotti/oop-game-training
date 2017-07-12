fb84a9b47dde8e11bb30507714d11c35df3a4572
ccpinotti <ccpinotti@gmail.com>
2017-07-11 18:32:04 -0700

    made headway on reset function and styling for the page

--- opp.js
+++ opp.js
@@ -1,32 +1,54 @@
 $(document).ready(function() {
+  var $turtle = $('#turtle');
+  var $rabbit = $('#rabbit');
+
 
   $(document).keypress(function(e) {
     if (e.key === "a") {
       $("#turtle").animate({
         left: '+=25px'
       });
+      if (parseInt($turtle.css('left')) >= 1000){
+        $('h2').text('The Turtle Wins!!');
+
+      }
     } else if (e.key === "l") {
       $("#rabbit").animate({
         left: '+=25px'
       });
+      if (parseInt($rabbit.css('left')) >= 1000){
+        $('h2').text('The Rabbit Wins!!');
+      }
     }
-    function reload2() {
-      location.reload();
-    }
-    var el = document.getElementById("Restart");
-    el.onclick = reload;
-
-    // document.getElementById("Reset").onclick = function() {
-    //   document.getElementById("button").value = "";
-    // };
-
-
-    //   $('button').on('click',function() {
-    //   $(".key").text("")
-    //
-    // });
-    // check window width
-    // compare width turtle/rabbit position
-    // check for winner
   });
+
+  function resetMe() {
+    $("#rabbit").css({
+      left: 0
+    });
+    $("#turtle").css({
+      left: 0
+    });
+    //reset other player
+  }
+
+  $("#reset").click(function() {
+
+    resetMe();
+  });
+  // el.onclick = reload();
+
+  // document.getElementById("Reset").onclick = function() {
+  //   document.getElementById("button").value = "";
+  // };
+
+
+  //   $('button').on('click',function() {
+  //   $(".key").text("")
+  //
+  // });
+  // check window width
+  // compare width turtle/rabbit position
+  // check for winner
+
 });
